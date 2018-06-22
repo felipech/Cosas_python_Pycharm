b08296b67e910c0fb6c8a2c601dff5eed25ac5e0
prueba <felip.choque@gmail.com>
2018-06-22 18:03:27 -0400

    empezando

--- Proyecto_Curso_Udemy/Proyecto1_Curso_Udemy.py
+++ Proyecto_Curso_Udemy/Proyecto1_Curso_Udemy.py
@@ -1,6 +1,11 @@
-
-
+class Usuario:
+    def __init__(self, nombre,edad, pais, email):
+        self.nombre = nombre
+        self.edad = edad
+        self.pais = pais
+        self.email = email
 
 
 print("Bienvenido al banco Python")
-print("prueba de pycharm")
+print("¿Desea crear una nueva cuenta en el banco?, presione 1")
+print("Si ya tiene una cuenta presionar 2 para acceder a esta")
